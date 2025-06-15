2fe79b6d0d9922bfb0ea9741e86e16d63d59c3d9
weiz-me <weizhang45name@gmail.com>
2025-06-15 00:04:08 -0400

    update link

--- pages/5 PP-🏢Job Application Tracker.py
+++ pages/5 PP-🏢Job Application Tracker.py
@@ -28,12 +28,12 @@ A full-stack web application designed to help users track and manage job applica
 
 st.write("""
 ### Demo Website: 
-#### Frontend: http://18.118.200.220:3000   
+#### Frontend: https://tracking-job-ap-frontend.vercel.app
 """)
-st.components.v1.iframe("http://18.118.200.220:3000", width=1200, height=800, scrolling=True)
+st.components.v1.iframe("https://tracking-job-ap-frontend.vercel.app", width=1200, height=800, scrolling=True)
 
 
 st.write("""
-#### Backend: http://18.118.200.220:5000
+#### Backend: https://tracking-job-ap-backend.onrender.com/
 """)
-st.components.v1.iframe("http://18.118.200.220:5000", width=1200, height=800, scrolling=True)
+st.components.v1.iframe("https://tracking-job-ap-backend.onrender.com/", width=1200, height=800, scrolling=True)
